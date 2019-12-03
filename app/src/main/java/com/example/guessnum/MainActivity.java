97e040c2039fb6da2bc669a5c941f7c20d8f1ed3
piskelee <dimos.lee@gmail.com>
2019-12-03 15:52:43 +0800

    ver3

--- app/src/main/java/com/example/guessnum/MainActivity.java
+++ app/src/main/java/com/example/guessnum/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.guessnum;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void inputNumber(View view) {
+    }
+
+    public void replay(View view) {
+    }
+
+    public void send(View view) {
+    }
+
+    public void clear(View view) {
+    }
+
+    public void back(View view) {
     }
 }
